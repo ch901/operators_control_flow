4a6253dca824dd8bbf4948ef8c8c2be8352c7151
Chinwe Okpala <chinweamara34@gmail.com>
2023-02-19 21:46:03 -0500

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 //General Subjects - English, Mathematics
 
 
-const scienceSubjects = ['Physics', 'Chemistry', 'Biology', 'Technical Drawing'];
+/*const scienceSubjects = ['Physics', 'Chemistry', 'Biology', 'Technical Drawing'];
 const socialScienceSubjects = ['Accounting', 'Commerce', 'Marketing', 'Geography'];
 const artsSubjects = ['Government', 'Economics', 'Literature', 'History']
 const generalSubjects = ['English', 'Mathematics']
@@ -25,4 +25,27 @@ console.log(`${generalSubjects}, ${artsSubjects}`)
 }
 else{
 console.log(generalSubjects)
-}
+}*/
+
+//question5
+function FindPowerOfTwoNearestToNumber(number) {
+    let pwr = 0
+    let rise = 0
+  
+    //   while do loop
+    do {
+      pwr = Math.pow(2, rise)
+      rise++
+    } while (pwr < number)
+  
+    // if else statement
+    if (pwr - number > number - Math.pow(2, rise - 2)) {
+      pwr = Math.pow(2, rise - 2)
+    }
+      
+    return 'The number ' + pwr + ' is the power of 2 nearest to ' +number
+  }
+  
+  const res = FindPowerOfTwoNearestToNumber(40)
+  
+  console.log(res)
