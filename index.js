5d1544ba53c89b890b7d48334d1fd3130c989632
Chinwe Okpala <chinweamara34@gmail.com>
2023-02-23 20:57:13 -0500

    updates

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 //question3
 
-//Science Subjects - Physics, Chemistry, Biology, Technical Drawing 
-//Social Science Subjects - Accounting, Commerce, Marketing, Geography
-//Arts Subjects - Government, Economics, Literature, History
-//General Subjects - English, Mathematics
 
 
-/*const scienceSubjects = ['Physics', 'Chemistry', 'Biology', 'Technical Drawing'];
+const scienceSubjects = ['Physics', 'Chemistry', 'Biology', 'Technical Drawing'];
 const socialScienceSubjects = ['Accounting', 'Commerce', 'Marketing', 'Geography'];
 const artsSubjects = ['Government', 'Economics', 'Literature', 'History']
 const generalSubjects = ['English', 'Mathematics']
@@ -25,7 +21,7 @@ console.log(`${generalSubjects}, ${artsSubjects}`)
 }
 else{
 console.log(generalSubjects)
-}*/
+}
 
 //question5
 function FindPowerOfTwoNearestToNumber(number) {
