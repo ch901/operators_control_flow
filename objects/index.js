4608bec75859b654ac5a73d6be6e3e9ef4c365d2
Chinwe Okpala <chinweamara34@gmail.com>
2023-02-26 15:37:55 -0500

    updates

--- objects/index.js
+++ objects/index.js
@@ -1,23 +1,37 @@
 
 
 
-//question1
+// //question1
 
-const instagramPost = new Object();
-instagramPost.handleOfAuthor = 'mufasatundeednut';
-instagramPost.content = 'image';
-instagramPost.anImageLinkPostedByTheAuthorOfThePost = 'https://www.instagram.com/p/Co__YOWusPq/?utm_source=ig_web_copy_link';
+// const instagramPost = new Object();
+// instagramPost.handleOfAuthor = 'mufasatundeednut';
+// instagramPost.content = 'image';
+// instagramPost.anImageLinkPostedByTheAuthorOfThePost = 'https://www.instagram.com/p/Co__YOWusPq/?utm_source=ig_web_copy_link';
 
-instagramPost.numberOfViews = 117036;
-instagramPost.numberOfLikes = 116720;
-instagramPost.displayNumberOfViews = function(){
-    return this.numberOfViews;
+// instagramPost.numberOfViews = 117036;
+// instagramPost.numberOfLikes = 116720;
+// instagramPost.displayNumberOfViews = function(){
+//     return this.numberOfViews;
+// }
+
+// console.log(instagramPost)
+
+// //question2
+
+// console.log('handle of authur is ' + instagramPost.handleOfAuthor)
+
+// console.log('numberOfViews is ' + instagramPost.numberOfViews)
+
+function instagramPost(handleOfAuthor,  content, anImageLinkPostedByTheAuthorOfThePost, numberOfViews, numberOfLikes) {
+    this.handleOfAuthor = handleOfAuthor;
+    this.content = content;
+    this.anImageLinkPostedByTheAuthorOfThePost = anImageLinkPostedByTheAuthorOfThePost;
+    this.numberOfViews = numberOfViews;
+    this.numberOfLikes = numberOfLikes;
+
 }
 
-console.log(instagramPost)
+let marwanedev = new instagramPost('marwanedev', 'image', 'link', 9655, 83077);
+console.log (marwanedev)
 
-//question2
-
-console.log('handle of authur is ' + instagramPost.handleOfAuthor)
-
-console.log('numberOfViews is ' + instagramPost.numberOfViews)
+let mufasatundeednut = new instagramPost('mufasatundeednut', 'image', 'link', 117036, 116720);
